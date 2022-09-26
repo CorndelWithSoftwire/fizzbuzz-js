095d85b98451c498006b0a73da75020597291847
Daniel Garwood <daniel.garwood@bbc.co.uk>
2022-09-26 15:44:11 +0100

    completed fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,29 +2,26 @@
 
 // Here, we create our main function.
 function fizzbuzz() {
-    
+
     console.log('Welcome to FizzBuzz!');
 
     // Put your code here...
-    for (let x=1; x<=100; x++) {
-        //if (x % 3 == 0 && x % 5 == 0) {
-          //  console.log("FizzBuzz")
-        //}
 
+    for (let x = 1; x <= 100; x++) {
         arr = [];
-        if (x % 3 == 0){
-            vars.push("Fizz")
+        if (x % 3 == 0) {
+            arr.push("Fizz")
         }
-        if (x % 5 == 0){
-            vars.push("Buzz")
+        if (x % 5 == 0) {
+            arr.push("Buzz")
+        }
+        if (arr.length == 0) {
+            console.log(x)
         }
         else {
-            console.log(x)
-            }
+            console.log(arr.join(""))
         }
-
-    
-
+    }
+}
 // Now we run the main function...
 fizzbuzz();
-    }
