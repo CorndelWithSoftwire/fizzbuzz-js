e928096031f981e19a5fdaafe987b366dfcf0d2a
Daniel Garwood <daniel.garwood@bbc.co.uk>
2022-09-26 15:05:54 +0100

    added fizz buzz, and array

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,9 +7,24 @@ function fizzbuzz() {
 
     // Put your code here...
     for (let x=1; x<=100; x++) {
-        console.log(x);
-    }
-}
+        //if (x % 3 == 0 && x % 5 == 0) {
+          //  console.log("FizzBuzz")
+        //}
+
+        arr = [];
+        if (x % 3 == 0){
+            vars.push("Fizz")
+        }
+        if (x % 5 == 0){
+            vars.push("Buzz")
+        }
+        else {
+            console.log(x)
+            }
+        }
+
+    
 
 // Now we run the main function...
 fizzbuzz();
+    }
