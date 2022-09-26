4ec8e2fcba03eaf9f3a39fffb2eca955694535c8
Daniel Garwood <daniel.garwood@bbc.co.uk>
2022-09-26 13:57:00 +0100

    added for loop for outputting 1 to 100

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,8 +6,9 @@ function fizzbuzz() {
     console.log('Welcome to FizzBuzz!');
 
     // Put your code here...
-
-    
+    for (let x=1; x<=100; x++) {
+        console.log(x);
+    }
 }
 
 // Now we run the main function...
